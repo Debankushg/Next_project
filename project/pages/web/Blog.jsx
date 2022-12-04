922ad380a7e032af07c0cef67553c77f5ab749b0
DEBANKUSH GHOSH <debankushghosh@gmail.com>
2022-12-04 23:13:58 +0530

    SERVER SIDE Rendering has been done

--- project/pages/web/Blog.jsx
+++ project/pages/web/Blog.jsx
@@ -2,17 +2,17 @@ import Link from "next/link"
 import Navbar from "../../component/Navbars"
 import styles from "../../styles/Blog.module.css"
 import React ,{useEffect,useState} from "react"
-const Blog = () => {
+const Blog = (props) => {
 
-  const [blogs, setBlogs] = useState([]);
+  const [blogs, setBlogs] = useState(props.allBlogs);
 
-  useEffect(() => {
-    fetch("http://localhost:3000/api/blog").then((a)=>{
-      return a.json()})
-    .then((parsed) =>{
-    setBlogs(parsed);
-    })
-    },[])
+  // useEffect(() => {
+  //   fetch("http://localhost:3000/api/blog").then((a)=>{
+  //     return a.json()})
+  //   .then((parsed) =>{
+  //   setBlogs(parsed);
+  //   })
+  //   },[])
   
 
   return (
@@ -31,6 +31,13 @@ const Blog = () => {
       </div>
     </div>
   )
+}
+
+export async function getServerSideProps() {
+ let data = await fetch("http://localhost:3000/api/blog")
+ let allBlogs = await data.json();
+  
+  return { props: { allBlogs } }
 }
 
 export default Blog
