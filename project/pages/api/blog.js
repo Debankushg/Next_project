83304aba1c7ec2643a3d965651127f6054072056
DEBANKUSH GHOSH <debankushghosh@gmail.com>
2022-12-04 15:37:17 +0530

    Endpoint for API fetch has been made

--- project/pages/api/blog.js
+++ project/pages/api/blog.js
@@ -1,9 +1,18 @@
 import * as fs from'fs'
 
 
-export default function handler(req, res) {
-  fs.readdir(`blogdata` ,'utf-8', (err,data)=>{
-      console.log(data);
-    res.status(200).json(data)
-  })
+export default async function handler(req, res) {
+
+  let data = await fs.promises.readdir(`blogdata` );
+  let myfile;
+  let allBlogs =[];
+    for (let index = 0; index < data.length; index++) {
+      const item = data[index];
+      myfile = await fs.promises.readFile(("blogdata/" + item),'utf-8')
+      allBlogs.push(JSON.parse(myfile))
+    }
+        
+     
+  res.status(200).json(allBlogs)
+
 }
